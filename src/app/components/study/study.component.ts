f0a92450f158fc72c8058ba21fc414dafc423961
Ackrom <marco.at720@gmail.com>
2018-11-17 12:57:27 -0430

    Visualizacion por tiles V1.0.0

--- src/app/components/study/study.component.ts
+++ src/app/components/study/study.component.ts
@@ -12,19 +12,49 @@ export class StudyComponent implements OnInit {
   constructor(private curriculum:CurriculumService) { }
 
   private studies:Study[];
+  private columns:Study[][];
+  private active:boolean;
 
   ngOnInit() {
+    this.active = true;
     // this.curriculum.setDefaultStydies();
-    if(localStorage.getItem('studies') !== null)
+    if(localStorage.getItem('studies') !== null){
       this.studies = JSON.parse(localStorage.getItem('studies'));
+      this.columns = this.toColumns(this.studies);
+    }
     this.curriculum.getStudies()
     .subscribe((data)=>{
       // console.log("StudyComponent");
       // console.log(data);
       this.studies = data;
+      this.columns = this.toColumns(data);
       localStorage.setItem('studies',JSON.stringify(data));
     },
     (error)=>{console.log("StudyComponent");console.log(error);});
   }
+  toColumns(data:Study[]):Study[][]{
+    let size = data.length;
+    let output:Study[][] = new Array(3);
 
+    output[0] = new Array(0);
+    for(let i=0;i<Math.round(size/3);i++){
+      output[0].push(data[i]);
+    }
+
+    output[1] = new Array(0);
+    for(let i=Math.round(size/3);i<Math.round(size/3)*2;i++){
+      output[1].push(data[i]);
+    }
+    
+    output[2] = new Array(0);
+    for(let i=Math.round(size/3)*2;i<size;i++){
+      output[2].push(data[i]);
+    }
+
+    return output;
+  }
+
+  changeTab(){
+    this.active = !this.active;
+  }
 }
--- src/app/components/tecnology-and-tools/tecnology-and-tools.component.ts
+++ src/app/components/tecnology-and-tools/tecnology-and-tools.component.ts
@@ -12,20 +12,51 @@ export class TecnologyAndToolsComponent implements OnInit {
   constructor(private curriculum:CurriculumService) { }
 
   private toolsOrTecnologies:ToolOrTecnology[];
+  private columns:ToolOrTecnology[][];
+  private active:boolean;
 
   ngOnInit() {
-    if(localStorage.getItem('toolsOrTecnologies') !== null)
+    this.active = true;
+    if(localStorage.getItem('toolsOrTecnologies') !== null){
       this.toolsOrTecnologies = JSON.parse(localStorage.getItem('toolsOrTecnologies'));
+      this.columns = this.toColumns(this.toolsOrTecnologies);
+    }
     // this.curriculum.setDefaultTecnologyOrTool();
     this.curriculum.getToolsOrTecnologies()
     .subscribe((data)=>{
       // console.log("TecnologyAndToolsComponent");
       // console.log(data);
       this.toolsOrTecnologies = data;
-      
+      this.columns = this.toColumns(data);
       localStorage.setItem('toolsOrTecnologies',JSON.stringify(data));
     },
     (error)=>{console.log("TecnologyAndToolsComponent");console.log(error);});
+  }
+
+  toColumns(data:ToolOrTecnology[]):ToolOrTecnology[][]{
+    let size = data.length;
+    let output:ToolOrTecnology[][] = new Array(3);
+
+    output[0] = new Array(0);
+    for(let i=0;i<Math.round(size/3);i++){
+      output[0].push(data[i]);
+    }
+
+    output[1] = new Array(0);
+    for(let i=Math.round(size/3);i<Math.round(size/3)*2;i++){
+      output[1].push(data[i]);
+    }
+    
+    output[2] = new Array(0);
+    for(let i=Math.round(size/3)*2;i<size;i++){
+      output[2].push(data[i]);
+    }
+
+    return output;
+  }
+
+  changeTab(){
+    this.active = !this.active;
   }
 
 }
