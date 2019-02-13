edd0efb51a561a8493aab1edff576d10e34e596a
avicdro <avicdro@gmail.com>
2019-02-12 23:32:23 -0400

    Modificando el munu principal

--- src/app/models/mocks/mock-Projec.ts
+++ src/app/models/mocks/mock-Projec.ts
@@ -3,4 +3,8 @@ import { Project } from "../Project";
 export const PROJECT:Project[] =[
     {name:"Sistema simple PHP con MVC",description:"El sistema nace de un trabajo para la universidad, lo elaboré junto con un grupo de compañeros en el año 2016. Lo más destacable del proyecto es el uso del patrón MVC y la integración con librerías externas para resolver problemas particulares.",links:[{name:"GitHub",direction:"https://github.com/Ackrom/Sistema-simple-con-MVC"}]},
     {name:"To do list con React, Electron y Bootstrap",description:"Una to do list que combina varias herramientas.",links:[{name:"Blogger",direction:"https://showmycode.blogspot.com/2018/06/to-do-list-con-react-electron-y.html"}]},
+    {name:"Graficos en tiempo real con Angular 6 y Socket.io",description:"Práctica que emula una aplicación que usa gráficos actualizables en tiempo real.",links:[{name:"GitHub",direction:"https://github.com/Ackrom/Graficos-en-tiempo-real-con-Angular-6-y-Socket.io"}]},
+    {name:"Busca minas en Angular 6",description:"El clásico juego del buscaminas usando Angular 6.",links:[{name:"GitHub",direction:"https://github.com/Ackrom/Buscaminas-con-Angular-6"}]},
+    {name:"Trabajo práctico (TP) en PASCAL",description:"Uno de mis primeros trabajos prácticos en el lenguaje deprogramación PASCAL.",links:[{name:"GitHub",direction:"https://github.com/Ackrom/TP-Almacen-en-PASCAL"}]},
+    {name:"Simple chat con: React,Socket.io y nodejs",description:"Un chat sencillo creado usando sockets.",links:[{name:"GitHub",direction:"https://github.com/Ackrom/simple-chat-with-react-socket.io-and-nodejs"}]},
 ]
--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { faGithub,faHackerrank,faTwitter,faFacebook,faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-menu',
@@ -10,6 +11,12 @@ import { Observable } from 'rxjs';
 export class MenuComponent implements OnInit {
 
   constructor() { }
+
+  github = faGithub;
+  hr = faHackerrank;
+  twitter = faTwitter;
+  face = faFacebook;
+  link = faLinkedin;
   
   ngOnInit() { }
 
